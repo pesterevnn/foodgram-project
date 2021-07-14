0457709b423745cc5a5e15a0c3aea80e7bade7b5
Николай Пестерев <pesnikon@yandex.ru>
2021-07-14 09:25:57 +0400

    fix psw reset

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -129,3 +129,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated', 
     )
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
